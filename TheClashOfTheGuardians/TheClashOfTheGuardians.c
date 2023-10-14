ae7dd4cdb58eb066e526e17694be6b8504434441
JoaquinMunoz0 <munosjoaquin0@gmail.com>
2023-10-14 17:15:45 -0300

    The Clash Of The Guardians v1.0
    Se cambio la estructura para el uso de pilas, por lo que decidí empezar todo desde 0, ahora la función para cargar los datos desde archivos puede cargar nombres con espacios incluidos, al crear nuevas cartas estas se agregan a la pila, ahora solo falta la lógica del juego por implementar y el historial, sin mencionar algunas validaciones que me están faltando.

--- TheClashOfTheGuardians/TheClashOfTheGuardians.c
+++ TheClashOfTheGuardians/TheClashOfTheGuardians.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <conio.h>
+#include <stdbool.h>
 
 struct Guardian
 {
@@ -11,68 +11,113 @@ struct Guardian
     int PV;
     int PA;
     int PD;
+    struct Guardian* siguiente;
 };
 
-struct Partida
+typedef struct Guardian Guardian;
+
+Guardian* crearGuardian(char nombre[], int PV, int PA, int PD, char tipo[])
 {
-    char ganador[50];
-    int puntosJugador1;
-    int puntosJugador2;
-};
-
-void mostrarMenu();
-void crearNuevaCarta(struct Guardian cartas[], int *numCartas);
-void cargarCartasDesdeArchivo(struct Guardian cartas[], int *numCartas);
-void iniciarPartida(struct Guardian cartas[], struct Guardian manoJugador[], struct Guardian manoComputadora[], int *numCartas, int *vidasJugador, int *vidasComputadora);
-void mostrarMano(struct Guardian mano[], int numCartas);
-int seleccionarGuardianAleatorio(int numCartas);
-
-int main()
-{
-    struct Guardian cartas[60];
-    struct Guardian manoJugador[3];
-    struct Guardian manoComputadora[3];
-    int numCartas = 0;
-    int vidasJugador = 5;
-    int vidasComputadora = 5;
-    srand(time(NULL));
-
-    int opcion;
-    do
-	{
-        mostrarMenu();
-        scanf("%d", &opcion);
-
-        switch (opcion)
-        {
-            case 1:
-                crearNuevaCarta(cartas, &numCartas);
-                break;
-            case 2:
-                iniciarPartida(cartas, manoJugador, manoComputadora, &numCartas, &vidasJugador, &vidasComputadora);
-                break;
-            case 3:
-                cargarCartasDesdeArchivo(cartas, &numCartas);
-                break;
-            case 4:
-                // Mostrar historial
-                break;
-            case 5:
-                printf("Cerrando el Juego...\n");
-                break;
-            default:
-                printf("Opcion no valida. Intentalo de nuevo.\n");
-                break;
-        }
-    } while (opcion != 5);
-
-    return 0;
+    Guardian* nuevoGuardian = (Guardian*)malloc(sizeof(Guardian));
+    if (nuevoGuardian == NULL)
+    {
+        printf("Error: No se pudo asignar Memoria para el nuevo guardian.\n");
+        exit(1);
+    }
+    strcpy(nuevoGuardian->nombre, nombre);
+    nuevoGuardian->PV = PV;
+    nuevoGuardian->PA = PA;
+    nuevoGuardian->PD = PD;
+    strcpy(nuevoGuardian->tipo, tipo);
+    nuevoGuardian->siguiente = NULL;
+    return nuevoGuardian;
 }
 
-//FUNCION PARA CARGAR CARTAS DESDE .TXT
-void cargarCartasDesdeArchivo(struct Guardian cartas[], int *numCartas) //COMPLETADA Y FUNCIONANDO
+//DECLARACION DE LA FUNCION MOSTRARCARTAS
+void mostrarCartas(Guardian* pila);
+
+//FUNCION PARA AGREGAR CARTAS A LA PILA.
+void push(Guardian** pila, Guardian* nuevoGuardian)
+{
+    nuevoGuardian->siguiente = *pila;
+    *pila = nuevoGuardian;
+}
+
+//FUNCION PARA EL MENU (NO HAY MUCHO QUE DECIR)
+void mostrarMenu()
+{
+    printf("Bienvenido a The Clash Of The Guardians!\n");
+    printf("1. Crear nueva carta\n");
+    printf("2. Jugar partida\n");
+    printf("3. Cargar cartas desde archivo\n");
+    printf("4. Ver Cartas Disponibles\n");
+    printf("5. Historial de la partida\n");
+    printf("6. Salir\n");
+    printf("Seleccione una opcion: ");
+}
+
+//FUNCION PARA CREAR UNA CARTA NUEVA Y AGREGARLA A LA PILA ACTUALIZADA Y FUNCIONANDO
+void crearNuevaCarta(Guardian** pila)
 {
     system("cls");
+    char nombre[60];
+    int PV, PA, PD;
+    int tipoNumero;
+
+    fflush(stdin);
+
+    printf("Ingrese el nombre del Guardian: ");
+    fgets(nombre, sizeof(nombre), stdin);
+    nombre[strcspn(nombre, "\n")] = 0;
+
+    printf("Ingrese los puntos de vida (PV): ");
+    scanf("%d", &PV);
+
+    printf("Ingrese los puntos de ataque (PA): ");
+    scanf("%d", &PA);
+
+    printf("Ingrese los puntos de defensa (PD): ");
+    scanf("%d", &PD);
+
+    printf("Seleccione el tipo de guardian:\n");
+    printf("1. Mago\n");
+    printf("2. Vikingo\n");
+    printf("3. Nigromante\n");
+    printf("4. Bestia\n");
+    scanf("%d", &tipoNumero);
+
+    char tipo[20];
+    switch (tipoNumero)
+    {
+    case 1:
+        strcpy(tipo, "Mago");
+        break;
+    case 2:
+        strcpy(tipo, "Vikingo");
+        break;
+    case 3:
+        strcpy(tipo, "Nigromante");
+        break;
+    case 4:
+        strcpy(tipo, "Bestia");
+        break;
+    default:
+        printf("Secreto Encontrado.\n");
+        strcpy(tipo, "Samurai");
+        break;
+    }
+
+    Guardian* nuevoGuardian = crearGuardian(nombre, PV, PA, PD, tipo);
+    push(pila, nuevoGuardian);
+
+    printf("Nueva carta creada y agregada al mazo.\nPresiona una tecla para continuar...");
+    getch();
+    system("cls");
+}
+
+//Cargar Cartas DESDE ARCHIVOS ACTUALIZADA Y FUNCIONANDO
+void cargarCartasDesdeArchivo(Guardian** pila)
+{
     FILE *archivo;
     char nombreArchivo[100];
     printf("Ingrese el nombre del archivo a cargar (incluyendo la extension .txt): ");
@@ -82,9 +127,6 @@ void cargarCartasDesdeArchivo(struct Guardian cartas[], int *numCartas) //COMPLE
     if (archivo == NULL)
     {
         printf("No se pudo abrir el archivo.\n");
-        printf("\nPresiona una tecla para continuar...");
-    	getch();
-    	system("cls");
         return;
     }
 
@@ -93,223 +135,99 @@ void cargarCartasDesdeArchivo(struct Guardian cartas[], int *numCartas) //COMPLE
     {
         char nombre[60], tipo[20];
         int PV, PA, PD;
-        sscanf(linea, "%[^,],%[^,],%d,%d,%d", nombre, tipo, &PV, &PA, &PD);
-        
-        strcpy(cartas[*numCartas].nombre, nombre);
-        strcpy(cartas[*numCartas].tipo, tipo);
-        cartas[*numCartas].PV = PV;
-        cartas[*numCartas].PA = PA;
-        cartas[*numCartas].PD = PD;
-
-        (*numCartas)++;
-        if(*numCartas >= 60)
+        if (sscanf(linea, "%[^,],%[^,],%d,%d,%d", nombre, tipo, &PV, &PA, &PD) == 5)
         {
-            printf("Ya se han cargado 60 cartas.\n");
-            break;
+            Guardian* nuevoGuardian = crearGuardian(nombre, PV, PA, PD, tipo);
+            push(pila, nuevoGuardian);
+        }
+        else
+        {
+            printf("Error encontrado en la Linea: %s\n", linea);
         }
     }
 
     fclose(archivo);
-    printf("Se han cargado las cartas desde el archivo.\n\n");
-    for(int i = 0; i < *numCartas; i++)
-    {
-        printf("Nombre: %s, Tipo: %s, PV: %d, PA: %d, PD: %d\n",
-               cartas[i].nombre, cartas[i].tipo, cartas[i].PV, cartas[i].PA, cartas[i].PD);
-    }
-    
     printf("\nPresiona una tecla para continuar...");
     getch();
     system("cls");
 }
 
-//FUNCION PARA CREAR CARTAS
-void crearNuevaCarta(struct Guardian cartas[], int *numCartas)//COMPLETADA Y FUNCIONANDO
+int main()
 {
-	system("cls");
-    if(*numCartas >= 60)
-	{
-        printf("Se ha alcanzado el limite de cartas.\n");
-        return;
-    }
+    Guardian* pila = NULL;
+    bool cartasDisponibles = false;
 
-    struct Guardian nuevaCarta;
-    printf("Ingrese el nombre del Guardian (sin espacios, máximo 59 caracteres): ");
-    scanf("%s", nuevaCarta.nombre);
-    printf("Ingrese los puntos de vida (PV): ");
-    scanf("%d", &nuevaCarta.PV);
-    printf("Ingrese los puntos de ataque (PA): ");
-    scanf("%d", &nuevaCarta.PA);
-    printf("Ingrese los puntos de defensa (PD): ");
-    scanf("%d", &nuevaCarta.PD);
+    int opcion;
+    do
+    {
+        mostrarMenu();
+        scanf("%d", &opcion);
 
-    if(strlen(nuevaCarta.nombre) > 59 || nuevaCarta.PV < 0 || nuevaCarta.PA < 0 || nuevaCarta.PD < 0)
-	{
-        printf("Los atributos del guardian son invalidos. Intentalo de nuevo.\n");
-        return;
-    }
-
-    printf("Seleccione el tipo de guardian:\n");
-    printf("1. Mago\n");
-    printf("2. Vikingo\n");
-    printf("3. Nigromante\n");
-    printf("4. Bestia\n");
-    int tipoGuardian;
-    scanf("%d", &tipoGuardian);
-    if(tipoGuardian < 1 || tipoGuardian > 4)
-	{
-        printf("Tipo de Guardian Inexistente.\n");
-        return;
-    }
-    
-    switch(tipoGuardian)
-	{
+        switch (opcion)
+        {
         case 1:
-            strcpy(nuevaCarta.tipo, "Mago");
+        	system("cls");
+            crearNuevaCarta(&pila);
+            cartasDisponibles = true;
             break;
         case 2:
-            strcpy(nuevaCarta.tipo, "Vikingo");
+            // Implementar jugar partida
             break;
         case 3:
-            strcpy(nuevaCarta.tipo, "Nigromante");
+        	system("cls");
+            cargarCartasDesdeArchivo(&pila);
+            cartasDisponibles = true;
             break;
         case 4:
-            strcpy(nuevaCarta.tipo, "Bestia");
+        	system("cls");
+            if (cartasDisponibles)
+			{
+                mostrarCartas(pila);
+            } else
+			{
+                printf("No hay cartas disponibles.\n");
+            }
+            printf("\nPresiona una tecla para continuar...");
+    		getch();
+    		system("cls");
+            break;
+        case 5:
+            // Implementar historial de la partida
+            break;
+        case 6:
+            printf("Cerrando el Juego...\n");
             break;
         default:
-            printf("Tipo de guardian Inexistente.\n");
-            return;
+            printf("Opcion no valida. Intentalo de nuevo.\n");
+            break;
+        }
+    } while (opcion != 6);
+
+    // Liberar cartas de la pila
+    Guardian* temp;
+    while (pila != NULL)
+    {
+        temp = pila;
+        pila = pila->siguiente;
+        free(temp);
     }
 
-    cartas[*numCartas] = nuevaCarta;
-    (*numCartas)++;
-    printf("Carta creada exitosamente.\n");
-    printf("\nPresiona una tecla para continuar...");
-    getch();
-    system("cls");
+    return 0;
 }
 
-//FUNCION MOSTRAR MENU
-void mostrarMenu()//COMPLETA Y FUNCIONANDO (SERIA RARO QUE NO FUNCIONARA PERO BUENO)
+//FUNCION PARA MOSTRAR LAS CARTAS DE LA PILA
+void mostrarCartas(Guardian* pila)
 {
-    printf("Bienvenido a The Clash Of The Guardians!\n");
-    printf("1. Crear nueva carta\n");
-    printf("2. Jugar partida\n");
-    printf("3. Cargar cartas desde archivo\n");
-    printf("4. Historial de la partida\n");
-    printf("5. Salir\n");
-    printf("Seleccione una opcion: ");
-}
+    Guardian* temp = pila;
+    int contador = 1;
 
-//FUNCION ENCARGADA DEL BUCLE DEL JUEGO
-void iniciarPartida(struct Guardian cartas[], struct Guardian manoJugador[], struct Guardian manoComputadora[], int *numCartas, int *vidasJugador, int *vidasComputadora)//AUN FALTAN DETALLES
-{
-	system("cls");
-    for(int i = 0; i < 3; i++)
-	{
-        int indiceAleatorio = rand() % *numCartas;
+    printf("Cartas en la pila:\n");
+    while (temp != NULL)
+    {
+        printf("%d. Nombre: %s, Tipo: %s, PV: %d, PA: %d, PD: %d\n",
+               contador, temp->nombre, temp->tipo, temp->PV, temp->PA, temp->PD);
 
-        manoJugador[i] = cartas[indiceAleatorio];
-        for(int j = indiceAleatorio; j < *numCartas - 1; j++)
-		{
-            cartas[j] = cartas[j + 1];
-        }
-        (*numCartas)--;
-
-        indiceAleatorio = rand() % *numCartas;
-        manoComputadora[i] = cartas[indiceAleatorio];
-        for (int j = indiceAleatorio; j < *numCartas - 1; j++)
-		{
-            cartas[j] = cartas[j + 1];
-        }
-        (*numCartas)--;
+        temp = temp->siguiente;
+        contador++;
     }
-    
-    *vidasJugador = 100;
-    *vidasComputadora = 100;
-
-    printf("La Partida a comenzado. El Jugador y la Computadora han recibido sus cartas.\n");
-
-    int turno = 1;
-    while(*vidasJugador > 0 && *vidasComputadora > 0)
-	{
-        printf("\nEstado actual de la partida:\n");
-        printf("Vidas del Jugador: %d\n", *vidasJugador);
-        printf("Vidas de la Computadora: %d\n", *vidasComputadora);
-
-        if (turno == 1)
-		{
-            printf("\nTurno del Jugador:\n");
-            mostrarMano(manoJugador, 3);
-            int seleccionGuardian;
-            do
-			{
-                printf("Que Guardian enviaras al campo de batalla? (1-3): ");
-                scanf("%d", &seleccionGuardian);
-            } while (seleccionGuardian < 1 || seleccionGuardian > 3);
-            
-            int dano = manoJugador[seleccionGuardian - 1].PA - manoComputadora[0].PD;
-            if(dano < 0)
-			{
-                dano = 0;
-            }
-
-            printf("¡%s ataca a la Computadora por %d puntos de dano!\n", manoJugador[seleccionGuardian - 1].nombre, dano);
-            *vidasComputadora -= dano;
-
-            if(*vidasComputadora <= 0)
-			{
-                printf("¡%s ha derrotado al Guardian de la Computadora!\n", manoJugador[seleccionGuardian - 1].nombre);
-                break;
-            }
-        }
-        
-        else
-		{
-            printf("\nTurno de la Computadora:\n");
-            int seleccionGuardian = seleccionarGuardianAleatorio(3);
-            int dano = manoComputadora[seleccionGuardian].PA - manoJugador[0].PD;
-            if(dano < 0)
-			{
-                dano = 0;
-            }
-
-            printf("¡%s ataca al Jugador por %d puntos de dano!\n", manoComputadora[seleccionGuardian].nombre, dano);
-            *vidasJugador -= dano;
-
-            if(*vidasJugador <= 0)
-			{
-                printf("¡%s ha derrotado al guardian del Jugador!\n", manoComputadora[seleccionGuardian].nombre);
-                break;
-            }
-        }
-        
-        turno = (turno == 1) ? 2 : 1;
-    }
-
-    if(*vidasJugador <= 0)
-	{
-        printf("\nLa Computadora ha ganado la partida\n");
-    }
-	else
-	{
-        printf("\nHas ganado la partida\n");
-    }
-    printf("\nPresiona una tecla para continuar...");
-    getch();
-    system("cls");
-}
-
-//FUNCION ENCARGADA DE MOSTAR MANO
-void mostrarMano(struct Guardian mano[], int numCartas)
-{
-    printf("Tus guardianes en mano:\n");
-    for(int i = 0; i < numCartas; i++)
-	{
-        printf("%d. Nombre: %s, PV: %d, PA: %d, PD: %d, Tipo: %s\n", i + 1, mano[i].nombre, mano[i].PV, mano[i].PA, mano[i].PD, mano[i].tipo);
-    }
-}
-
-int seleccionarGuardianAleatorio(int numGuardianes)
-{
-    return rand() % numGuardianes;
 }
