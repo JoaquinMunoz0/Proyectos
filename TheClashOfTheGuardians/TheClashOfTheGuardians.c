188966a1171b83f2b0668244c4a10518378efed6
JoaquinMunoz0 <munosjoaquin0@gmail.com>
2023-10-14 23:14:05 -0300

    The Clash Of The Guardians v1.5
    Se implementaron las funciones revolver el mazo y repartir las cartas,
    falta el control de los turnos, las acciones del jugador y los cálculos para los daños y todo lo ademas.

--- TheClashOfTheGuardians/TheClashOfTheGuardians.c
+++ TheClashOfTheGuardians/TheClashOfTheGuardians.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <conio.h>
 #include <stdbool.h>
+#include <time.h>
 
 struct Guardian
 {
@@ -33,14 +34,95 @@ Guardian* crearGuardian(char nombre[], int PV, int PA, int PD, char tipo[])
     return nuevoGuardian;
 }
 
-//DECLARACION DE LA FUNCION MOSTRARCARTAS
+//DECLARACION DE FUNCIONES
+void mostrarMenu();
+void crearNuevaCarta(Guardian** pila);
+void cargarCartasDesdeArchivo(Guardian** pila);
 void mostrarCartas(Guardian* pila);
+void revolverCartas(Guardian** pila);
+int repartirCartas(Guardian** pila, Guardian** pilaJugador, Guardian** pilaComputadora);
 
-//FUNCION PARA AGREGAR CARTAS A LA PILA.
-void push(Guardian** pila, Guardian* nuevoGuardian)
+void push(Guardian** pila, Guardian* nuevaCarta);
+Guardian* pop(Guardian** mazo);
+
+int main()
 {
-    nuevoGuardian->siguiente = *pila;
-    *pila = nuevoGuardian;
+    Guardian* pila = NULL;
+	Guardian* pilaJugador = NULL;
+	Guardian* pilaComputadora = NULL;
+    bool cartasDisponibles = false;
+
+    int opcion;
+    do
+    {
+        mostrarMenu();
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+        	system("cls");
+            crearNuevaCarta(&pila);
+            cartasDisponibles = true;
+            break;
+        case 2:
+		    system("cls"); // Limpiar la pantalla
+		    if (cartasDisponibles)
+			{
+		        revolverCartas(&pila); // Llamar a la función para revolver las cartas
+		        printf("Las cartas han sido revueltas ");
+		        repartirCartas(&pila, &pilaJugador, &pilaComputadora);
+		        printf("y repartidas\nAqui esta tu mazo:\n");
+		        mostrarCartas(pilaJugador);
+		        // Implementar lógica del juego y enfrentamientos aquí
+		    } else
+			{
+		        printf("No hay cartas disponibles para jugar. Crea nuevas cartas o carga desde un archivo.\n");
+		    }
+		    printf("\nPresiona una tecla para continuar...");
+		    getch();
+		    system("cls");
+		    break;
+        case 3:
+        	system("cls");
+            cargarCartasDesdeArchivo(&pila);
+            cartasDisponibles = true;
+            break;
+        case 4:
+        	system("cls");
+            if (cartasDisponibles)
+			{
+                mostrarCartas(pila);
+            } else
+			{
+                printf("No hay cartas disponibles.\n");
+            }
+            printf("\nPresiona una tecla para continuar...");
+    		getch();
+    		system("cls");
+            break;
+        case 5:
+            // Implementar historial de la partida
+            break;
+        case 6:
+            printf("Cerrando el Juego...\n");
+            break;
+        default:
+            printf("Opcion no valida. Intentalo de nuevo.\n");
+            break;
+        }
+    } while (opcion != 6);
+
+    // Liberar cartas de la pila
+    Guardian* temp;
+    while (pila != NULL)
+    {
+        temp = pila;
+        pila = pila->siguiente;
+        free(temp);
+    }
+
+    return 0;
 }
 
 //FUNCION PARA EL MENU (NO HAY MUCHO QUE DECIR)
@@ -127,6 +209,9 @@ void cargarCartasDesdeArchivo(Guardian** pila)
     if (archivo == NULL)
     {
         printf("No se pudo abrir el archivo.\n");
+        printf("\nPresiona una tecla para continuar...");
+    	getch();
+    	system("cls");
         return;
     }
 
@@ -147,81 +232,39 @@ void cargarCartasDesdeArchivo(Guardian** pila)
     }
 
     fclose(archivo);
-    printf("\nPresiona una tecla para continuar...");
+    printf("\nSe han cargado las cartas\nPresiona una tecla para continuar...");
     getch();
     system("cls");
 }
 
-int main()
+//FUNCION PARA AGREGAR CARTAS A LA PILA
+void push(Guardian** pila, Guardian* nuevaCarta)
 {
-    Guardian* pila = NULL;
-    bool cartasDisponibles = false;
-
-    int opcion;
-    do
-    {
-        mostrarMenu();
-        scanf("%d", &opcion);
-
-        switch (opcion)
-        {
-        case 1:
-        	system("cls");
-            crearNuevaCarta(&pila);
-            cartasDisponibles = true;
-            break;
-        case 2:
-            // Implementar jugar partida
-            break;
-        case 3:
-        	system("cls");
-            cargarCartasDesdeArchivo(&pila);
-            cartasDisponibles = true;
-            break;
-        case 4:
-        	system("cls");
-            if (cartasDisponibles)
-			{
-                mostrarCartas(pila);
-            } else
-			{
-                printf("No hay cartas disponibles.\n");
-            }
-            printf("\nPresiona una tecla para continuar...");
-    		getch();
-    		system("cls");
-            break;
-        case 5:
-            // Implementar historial de la partida
-            break;
-        case 6:
-            printf("Cerrando el Juego...\n");
-            break;
-        default:
-            printf("Opcion no valida. Intentalo de nuevo.\n");
-            break;
-        }
-    } while (opcion != 6);
-
-    // Liberar cartas de la pila
-    Guardian* temp;
-    while (pila != NULL)
-    {
-        temp = pila;
-        pila = pila->siguiente;
-        free(temp);
-    }
-
-    return 0;
+    nuevaCarta->siguiente = *pila; // La siguiente carta en el pila es la actual primera carta
+    *pila = nuevaCarta; // La nueva carta se convierte en la primera carta del pila
 }
 
-//FUNCION PARA MOSTRAR LAS CARTAS DE LA PILA
+//FUNCION PARA QUITAR CARTAS A LA PILA
+Guardian* pop(Guardian** mazo)
+{
+    if (*mazo == NULL) {
+        printf("El mazo está vacío.\n");
+        return NULL;
+    }
+
+    Guardian* carta = *mazo; // Obtén la primera carta del mazo
+    *mazo = (*mazo)->siguiente; // Actualiza el mazo para excluir la primera carta
+    carta->siguiente = NULL; // Marca la siguiente carta como NULL para evitar problemas
+
+    return carta;
+}
+
+//FUNCION PARA MOSTRAR LAS CARTAS COMPLETAS
 void mostrarCartas(Guardian* pila)
 {
     Guardian* temp = pila;
     int contador = 1;
 
-    printf("Cartas en la pila:\n");
     while (temp != NULL)
     {
         printf("%d. Nombre: %s, Tipo: %s, PV: %d, PA: %d, PD: %d\n",
@@ -230,4 +273,60 @@ void mostrarCartas(Guardian* pila)
         temp = temp->siguiente;
         contador++;
     }
+}
+
+//FUNCION PARA REVOLVER LAS CARTAS
+void revolverCartas(Guardian** pila)
+{
+    // Convertir la pila en un array
+    Guardian* cartasArray[60]; // Suponiendo un máximo de 60 cartas
+    int numCartas = 0;
+    Guardian* temp = *pila;
+    while (temp != NULL) {
+        cartasArray[numCartas] = temp;
+        temp = temp->siguiente;
+        numCartas++;
+    }
+
+    // Revolver el array
+    srand(time(NULL));
+    for (int i = numCartas - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        Guardian* temp = cartasArray[i];
+        cartasArray[i] = cartasArray[j];
+        cartasArray[j] = temp;
+    }
+
+    // Volver a colocar las cartas en la pila
+    *pila = NULL;
+    for (int i = 0; i < numCartas; i++) {
+        cartasArray[i]->siguiente = *pila;
+        *pila = cartasArray[i];
+    }
+}
+
+//FUNCION PARA REPARTIR LAS CARTAS
+int repartirCartas(Guardian** pila, Guardian** pilaJugador, Guardian** pilaComputadora)
+{
+    int contador = 1;
+    Guardian* temp = *pila;
+    while (temp != NULL && contador <= 30) {
+        Guardian* carta = crearGuardian(temp->nombre, temp->PV, temp->PA, temp->PD, temp->tipo);
+        if (contador <= 15) {
+            push(pilaJugador, carta); // Agrega las primeras 15 cartas al jugador
+        } else {
+            push(pilaComputadora, carta); // Agrega las siguientes 15 cartas a la computadora
+        }
+        temp = temp->siguiente;
+        contador++;
+    }
+
+    // Elimina las cartas repartidas del pila principal
+    for (int i = 1; i <= 30; i++) {
+        Guardian* temp = *pila;
+        *pila = (*pila)->siguiente;
+        free(temp);
+    }
+
+    return contador - 1; // Devuelve el número total de cartas repartidas
 }
